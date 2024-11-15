cd3ab3491aa8312da5383b7cc43eba69f2e18b9f
fufidx <89090248630ga@gmail.com>
2024-11-15 14:57:15 +0500

    Второй коммит

--- main.cpp
+++ main.cpp
@@ -1,10 +1,39 @@
 #include <iostream>
 #include <vector>
 
+
+
 using namespace std;
 
+
+void readfile(vector<string> stroki)
+{
+
+}
+void writefile(vector<string> stroki)
+{
+
+}
+void countfile(vector<string> stroki)
+{
+
+}
+void sortfile(vector<string> stroki)
+{
+
+}
+void printfile(vector<string> stroki)
+{
+
+}
 int main()
 {
+     vector<string> stroki{};
+     readfile(stroki);
+     writefile(stroki);
+     countfile(stroki);
+     sortfile(stroki);
+     printfile(stroki);
 
     return 0;
 }
